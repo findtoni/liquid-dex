a2254a1cb1b5f7f59c6049c5b0911284b72af467
findtoni <thatmusicjunkie@gmail.com>
2022-02-16 19:02:09 +0100

    feat: delegated token transfers/approval

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -10,8 +10,10 @@ contract Token {
   uint256 public decimals = 18;
   uint256 public totalSupply;
   mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
 
   event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 
   constructor() public {
     totalSupply = 1000000 * (10 ** decimals);
@@ -19,11 +21,27 @@ contract Token {
   }
 
   function transfer(address _to, uint256 _value) public returns (bool success) {
-    require(_to != address(0));
     require(balanceOf[msg.sender] >= _value);
-    balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+    _transfer(msg.sender, _to, _value);
+    return true;
+  }
+  function _transfer(address _from, address _to, uint256 _value) internal {
+    require(_to != address(0));
+    balanceOf[_from] = balanceOf[_from].sub(_value);
     balanceOf[_to] = balanceOf[_to].add(_value);
-    emit Transfer(msg.sender, _to, _value);
+    emit Transfer(_from, _to, _value);
+  }
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    require(_spender != address(0));
+    allowance[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    require(_value <= balanceOf[_from]);
+    require(_value <= allowance[_from][msg.sender]);
+    allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+    _transfer(_from, _to, _value);
     return true;
   }
 }
