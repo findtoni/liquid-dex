a22f61cb7e8cbd71c9fae354991911faf7e0dfb5
findtoni <thatmusicjunkie@gmail.com>
2022-02-16 22:25:45 +0100

    chore: added ether helper

--- helpers.js
+++ helpers.js
@@ -1,5 +1,4 @@
-export const tokens = (n) => {
-  return new web3.utils.BN(web3.utils.toWei(n.toString(), 'ether'));
-}
-
+export const ether = (n) => new web3.utils.BN(web3.utils.toWei(n.toString(), 'ether'));
 export const EVM_REVERT = 'VM Exception while processing transaction: revert';
+export const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000';
+export const tokens = (n) => ether(n);
