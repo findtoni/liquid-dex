45ca45654f3ce0c3c824ec5124df998b2e3aafe4
findtoni <thatmusicjunkie@gmail.com>
2022-02-17 00:38:44 +0100

    feat: added make/cancel order functions

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -11,9 +11,40 @@ contract Exchange {
   uint256 public feePercent;
   address constant ETHER = address(0);
   mapping(address => mapping(address => uint256)) public tokens;
+  mapping(uint256 => _Order) public orders; // store all orders
+  uint256 public orderCount;
+  mapping(uint256 => bool) public orderCancelled;
 
   event Deposit(address token, address user, uint256 amount, uint256 balance);
   event Withdraw(address token, address user, uint256 amount, uint256 balance);
+  event Order(
+    uint256 id,
+    address user,
+    address tokenGet,
+    uint256 amountGet,
+    address tokenGive,
+    uint256 amountGive,
+    uint256 timestamp
+  );
+  event Cancel(
+    uint256 id,
+    address user,
+    address tokenGet,
+    uint256 amountGet,
+    address tokenGive,
+    uint256 amountGive,
+    uint256 timestamp
+  );
+
+  struct _Order {
+    uint256 id;
+    address user;
+    address tokenGet;
+    uint amountGet;
+    address tokenGive;
+    uint amountGive;
+    uint timestamp;
+  }
 
   constructor(address _feeAccount, uint256 _feePercent) {
     feeAccount = _feeAccount;
@@ -48,5 +79,17 @@ contract Exchange {
   }
   function balanceOf(address _token, address _user) public view returns (uint256) {
     return tokens[_token][_user];
+  }
+  function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+    orderCount = orderCount.add(1);
+    orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
+    emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
+  }
+  function cancelOrder(uint256 _id) public {
+    _Order storage _order = orders[_id]; // fetch order from orders mapping
+    require(address(_order.user) == msg.sender);
+    require(_order.id == _id);
+    orderCancelled[_id] = true;
+    emit Cancel(_order.id, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, block.timestamp);
   }
 }
