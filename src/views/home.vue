7a1aeeae9e2d75b1620c6437e3a9a694241072a9
findtoni <thatmusicjunkie@gmail.com>
2022-04-17 15:05:43 +0100

    feat: main layout

---
+++ src/views/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="w-full flex flex-col">
+    <AHeader />
+    <ATokenBar />
+    <div class="flex justify-between items-center">
+      <div class="w-1/4 bg-purple-300">
+        <AMarkets />
+      </div>
+      <div class="w-2/4 flex flex-col bg-purple-500">
+        <ACharts />
+        <APlaceOrder />
+      </div>
+      <div class="w-1/4 flex flex-col bg-purple-300">
+        <AOrderBook />
+        <ATrades />
+      </div>
+    </div>
+    <AOpenOrders />
+    <AOrderHistory />
+  </section>
+</template>
+
+<script setup>
+</script>
