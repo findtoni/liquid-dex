70126678efff1831051bb66bdfcb16204304200e
findtoni <thatmusicjunkie@gmail.com>
2022-05-02 11:35:39 +0100

    feat: views wip

---
+++ src/components/AHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="py-5 bg-sky-900 text-white">
+    <div class="container">
+      <div class="flex justify-between items-center">
+        <h1>Liquid</h1>
+        <div class="flex justify-end items-center">
+          <p>{{ store.account }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useStore } from '../store';
+const store = useStore();
+</script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,22 +1,24 @@
 <template>
-  <section class="w-full flex flex-col">
-    <AHeader />
-    <ATokenBar />
-    <div class="flex justify-between items-center">
-      <div class="w-1/4 bg-purple-300">
-        <AMarkets />
+  <section class="main">
+    <div class="w-full flex flex-col">
+      <AHeader />
+      <ATokenBar />
+      <div class="flex justify-between items-center">
+        <div class="w-1/4 bg-purple-300">
+          <AMarkets />
+        </div>
+        <div class="w-2/4 flex flex-col bg-purple-500">
+          <ACharts />
+          <APlaceOrder />
+        </div>
+        <div class="w-1/4 flex flex-col bg-purple-300">
+          <AOrderBook />
+          <ATrades />
+        </div>
       </div>
-      <div class="w-2/4 flex flex-col bg-purple-500">
-        <ACharts />
-        <APlaceOrder />
-      </div>
-      <div class="w-1/4 flex flex-col bg-purple-300">
-        <AOrderBook />
-        <ATrades />
-      </div>
+      <AOpenOrders />
+      <AOrderHistory />
     </div>
-    <AOpenOrders />
-    <AOrderHistory />
   </section>
 </template>
 
