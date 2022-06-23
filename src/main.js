d9e9086eac451271948750c1d1f9dfb8fb64e2ce
Toni <35571969+findtoni@users.noreply.github.com>
2022-06-23 21:38:18 +0100

    feat: added packages

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import app from './app.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import VueApexCharts from 'vue3-apexcharts';
+import 'ant-design-vue/lib/message/style/index.css';
 import './assets/styles/tailwind.css'
+import './assets/global.scss'
 
 createApp(app)
   .use(createPinia())
   .use(router)
+  .use(VueApexCharts)
   .mount('#app')
