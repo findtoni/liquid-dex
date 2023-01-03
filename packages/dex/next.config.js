08f1b33246840eadcf16a60f0bbbe3f75bb626e2
Toni <35571969+findtoni@users.noreply.github.com>
2023-01-03 08:08:08 +0100

    chore: configure dex package
    - add dependencies
    - config typescript, tailwind

--- packages/dex/next.config.js
+++ packages/dex/next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.shields.io",
+        port: "",
+        pathname: "/github/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
