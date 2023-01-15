99a73b35b9c80b78ea9c4cafb97db664eaf8c163
Toni <35571969+findtoni@users.noreply.github.com>
2023-01-15 17:00:41 +0100

    feat: added chain/token constants & utils

---
+++ packages/dex/constants/tokenLists.ts
@@ -0,0 +1,10 @@
+const UNISWAP_LIST = 'https://tokens.uniswap.org';
+const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json';
+export const ARBITRUM_LIST = 'https://bridge.arbitrum.io/token-list-42161.json';
+export const OPTIMISM_LIST = 'https://static.optimism.io/optimism.tokenlist.json';
+
+export const TOKEN_LISTS: string[] = [
+  UNISWAP_LIST,
+  ARBITRUM_LIST,
+  OPTIMISM_LIST,
+];
