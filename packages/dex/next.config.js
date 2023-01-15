6c716a13109ad095f781135217449c7d0afa6fc3
Toni <35571969+findtoni@users.noreply.github.com>
2023-01-15 16:53:43 +0100

    chore: next and lint config

--- packages/dex/next.config.js
+++ packages/dex/next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
+const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
   experimental: {
     appDir: true,
@@ -13,10 +13,46 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "img.shields.io",
-        port: "",
-        pathname: "/github/**",
+        protocol: 'https',
+        hostname: 'img.shields.io',
+        port: '',
+        pathname: '/github/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ipfs.io',
+        port: '',
+        pathname: '/ipfs/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tokens.1inch.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.coingecko.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 's2.coinmarketcap.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ethereum-optimism.github.io',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
