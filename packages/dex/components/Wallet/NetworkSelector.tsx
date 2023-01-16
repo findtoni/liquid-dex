9ec28990f897204d00d0a698fb0da3e467de5003
Toni <35571969+findtoni@users.noreply.github.com>
2023-01-16 06:03:05 +0100

    fix: network/chain select

--- packages/dex/components/Wallet/NetworkSelector.tsx
+++ packages/dex/components/Wallet/NetworkSelector.tsx
@@ -11,6 +11,7 @@ import {
   MenuItem,
 } from '@chakra-ui/react';  
 import { useTradeStore } from '../../store/trade';
+import { useTokensStore } from '../../store/tokens';
 import { L1ChainInfo, L2ChainInfo, CHAIN_INFO, SupportedChainId } from '../../constants/chainInfo';
 
 function ChainLogo({ src, alt }: { src: string, alt: string }) {
@@ -19,6 +20,7 @@ function ChainLogo({ src, alt }: { src: string, alt: string }) {
 
 export default function NetworkSelector() {
   const trade = useTradeStore();
+  const { tokenLists, loadTokens } = useTokensStore();
   const chains = [
     CHAIN_INFO[SupportedChainId.MAINNET],
     CHAIN_INFO[SupportedChainId.GOERLI],
@@ -27,15 +29,24 @@ export default function NetworkSelector() {
   ];
   const activeChain = chains.find(chain => chain.chainId === trade.chain);
 
+  function switchChain(chainId: SupportedChainId) {
+    trade.setChain(chainId);
+    const tokenList = tokenLists[chainId]?.tokens;
+    trade.setTradeToken('sell', tokenList[0]);
+    trade.setTradeToken('buy', tokenList[1]);
+    trade.setTradeAmount('sell', 0);
+    trade.setTradeAmount('buy', 0);
+  }
+
   return (
     <Menu>
-      <MenuButton as={Button} leftIcon={<ChainLogo src={activeChain.logoUrl} alt={activeChain.label} />} 
+      <MenuButton as={Button} leftIcon={<ChainLogo src={activeChain.logoUrl} alt={activeChain.label} />}
         rightIcon={<ChevronDownIcon className="w-4 h-4" />}>
         { chains.find(chain => chain.chainId === trade.chain)?.label }
       </MenuButton>
       <MenuList>
         { chains.filter(chain => chain.chainId !== trade.chain)?.map((chain, index) =>
-          <MenuItem key={index} onClick={() => trade.setChain(chain.chainId)} 
+          <MenuItem key={index} onClick={() => switchChain(chain.chainId)} 
             icon={<ChainLogo src={chain.logoUrl} alt={chain.label} />}>
             { chain.label }
           </MenuItem>
