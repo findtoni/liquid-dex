0592ba0337c97aa4379fe96191be72603fa06d1f
Toni <35571969+findtoni@users.noreply.github.com>
2023-02-07 11:25:06 +0100

    fix: minor fix

--- packages/dex/hooks/useWagmi.ts
+++ packages/dex/hooks/useWagmi.ts
@@ -9,7 +9,7 @@ import { publicProvider } from 'wagmi/providers/public';
 
 export const chains = [mainnet, goerli, arbitrum, optimism];
 
-const projectId = `${process.env.WALLETCONNECT_PROJECT_ID}`;
+const projectId = `${process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID}`;
 
 const { provider } = configureChains(chains, [
   walletConnectProvider({ projectId: projectId }),
