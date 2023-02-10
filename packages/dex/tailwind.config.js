ca0e87dc45d5e23aa411a4cb05d9d6f276a0be33
Toni <35571969+findtoni@users.noreply.github.com>
2023-02-10 15:00:31 +0100

    fix: lint & style fixes

--- packages/dex/tailwind.config.js
+++ packages/dex/tailwind.config.js
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       container: {
         center: true,
         padding: {
-          DEFAULT: "1rem",
-          sm: "1rem",
-          lg: "1rem",
-          xl: "1rem",
-          "2xl": "1rem",
+          DEFAULT: '1rem',
+          sm: '1rem',
+          lg: '1rem',
+          xl: '1rem',
+          '2xl': '1rem',
         },
       },
       colors: {
-        primary: "#0B1628",
-        secondary: "#1C121E",
+        primary: '#0B1628',
+        secondary: '#1C121E',
       },
       fontFamily: {
-        sans: ["Nunito", "sans-serif"],
-        serif: ["Alegreya", "serif"],
+        sans: ['Nunito', 'sans-serif'],
+        serif: ['Alegreya', 'serif'],
       },
     },
   },
