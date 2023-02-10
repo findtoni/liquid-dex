3f3b33a68159dc4efca3ef641f6f695e49eab7c5
Toni <35571969+findtoni@users.noreply.github.com>
2023-02-10 15:07:59 +0100

    feat: trade prices and token balance

--- packages/dex/components/Swapper/TokenInput.tsx
+++ packages/dex/components/Swapper/TokenInput.tsx
@@ -7,96 +7,108 @@ import {
   NumberInputField,
 } from '@chakra-ui/react';
 import { ChevronDownIcon } from '@heroicons/react/24/solid';
-import TokenSelectorModal from './TokenSelectorModal';
+import TokenSelector from './TokenSelector';
 import TokenSkeleton from './TokenSkeleton';
 import { TokenInfo } from '@uniswap/token-lists';
-import { TokenImage } from './TokenSelectorModal';
+import { TokenImage } from './TokenSelector';
 import { useTradeStore } from '../../store/trade';
-import TokenQuote from './TokenQuote';
-
-type Token = {
-  token: TokenInfo;
-  amount: number;
-  price?: string;
-};
 
 interface TokenInput {
   type: 'buy' | 'sell';
-  token: Token;
+  token: {
+    token: TokenInfo;
+    amount: string;
+    price?: string;
+    balance?: string | null;
+  };
   onMax?: () => void;
   onReverse: () => void;
+  onInput?: ((value: string) => void) | undefined;
   loading?: boolean;
 }
 
-function TokenAmountInput({ type }: { type: 'buy' | 'sell' }) {
-  const [amount, setAmount] = useState<number>(0);
-  const { setTradeAmount } = useTradeStore();
-  //todo: set max to token max in wallet
-  
-  const handleChange = (value: number) => {
-    setAmount(value);
-    setTradeAmount(type, value);
-  };
+interface TokenInputAmount {
+  amount: string;
+  balance?: string | null;
+  onInput: ((value: string) => void) | undefined;
+}
+
+function TokenInputAmount({ amount, balance, onInput }: TokenInputAmount) {
 
   return (
-    //@ts-ignore
-    <NumberInput value={amount} onChange={handleChange} min={0} max={30} maxW='90px' allowMouseWheel>
-      <NumberInputField />
-    </NumberInput>
+    <div className="token-input">
+      <NumberInput
+        value={amount}
+        onChange={onInput}
+        maxW={16}
+        min={0}
+        max={Number(balance)}
+        variant="unstyled"
+        allowMouseWheel>
+        <NumberInputField />
+      </NumberInput>
+    </div>
   );
 }
 
-export default function TokenInput({ type, token, onMax, onReverse, loading }: TokenInput) {
+export default function TokenInput({ type, token, onMax, onReverse, onInput, loading }: TokenInput) {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
-      <div className="bg-slate-900 rounded-lg p-3 flex flex-col space-y-1.5">
+      <div className="bg-[#161c2f] rounded-lg py-3 px-4 flex flex-col space-y-1.5">
         {/* Token Balance / Set Max */}
         <div className="flex justify-between items-center text-gray-400">
-          <p className="text-sm">You {type}</p>
-          <div className="flex justify-between items-center space-x-1">
-            <p className="text-sm">Balance: 0</p>
-            {type == 'sell' ? (
-              <span
-                onClick={onMax}
-                className="p-0.5 bg-slate-800 rounded text-blue-300 text-xs uppercase font-medium">
-                max
-              </span>
-            ) : null}
-          </div>
+          <p className="text-sm font-medium">You {type}</p>
+          {type == 'sell'
+            ? loading
+              ? (<TokenSkeleton width="24" />)
+              : (
+                <div className="flex justify-between items-center space-x-1">
+                  <p className="m-0 p-0 text-xs">Balance: {token.balance}</p>
+                  <span
+                    onClick={onMax}
+                    className="p-1 bg-slate-800 rounded text-blue-300 text-[0.6rem] uppercase font-semibold">
+                    max
+                  </span>
+                </div>
+              )
+            : null}
         </div>
+
         {/* Token Select / Amount */}
         <div className="flex justify-between items-center text-white">
           <div
             onClick={onOpen}
-            className="flex justify-start items-center space-x-1 hover:cursor-pointer p-1 hover:rounded hover:bg-gray-600">
+            className="flex justify-start items-center space-x-2 hover:cursor-pointer p-1 hover:rounded hover:bg-gray-600">
             <TokenImage
               name={token.token?.name}
               logoURI={token.token?.logoURI}
             />
-            <p className="text-xl font-medium">{token.token?.symbol}</p>
-            <ChevronDownIcon className="text-white h-3 w-3" />
+            <div className="flex justify-between items-center space-x-1">
+              <p className="text-lg font-semibold">{token.token?.symbol}</p>
+              <ChevronDownIcon className="text-white h-3 w-3" />
+            </div>
           </div>
-          {loading ? (
+          {type === 'sell' ? (
+            <TokenInputAmount balance={token.balance} amount={token.amount} onInput={onInput} />
+          ) : loading ? (
             <TokenSkeleton width="24" />
-          ) : type === 'sell' ? (
-            <TokenAmountInput type={type} />
           ) : (
-            <TokenQuote />
+            <p className="text-lg font-semibold">{token.amount}</p>
           )}
         </div>
+
         {/* Token Name / Token Price */}
         <div className="flex justify-between items-center text-gray-400">
-          <p className="text-sm">{token.token.name}</p>
-          {loading ? (
-            <TokenSkeleton width="10" />
-          ) : (
-            <p className="text-sm">{`$ ${token?.price}`}</p>
-          )}
+          <p className="text-sm font-medium capitalize">{token.token?.name}</p>
+          {/* {loading
+            ? (<TokenSkeleton width="10" />)
+            : (<p className="text-sm">{`$ ${token?.price}`}</p>)
+          } */}
         </div>
       </div>
-      <TokenSelectorModal
+      <TokenSelector
         type={type}
         isOpen={isOpen}
         onClose={onClose}
--- packages/dex/components/Swapper/index.tsx
+++ packages/dex/components/Swapper/index.tsx
@@ -1,45 +1,103 @@
 'use client';
+
 import { useState } from 'react';
-import { Button } from '@chakra-ui/react';
 import { useAccount } from 'wagmi';
+import { useTradeStore } from '../../store/trade';
+import { Network, Alchemy } from 'alchemy-sdk';
+import qs from 'query-string';
+
+import { Button } from '@chakra-ui/react';
+import { InformationCircleIcon } from '@heroicons/react/24/solid';
 import Card from '../Card';
 import TokenInput from './TokenInput';
 import TokenReverse from './TokenReverse';
-import { useTradeStore } from '../../store/trade';
 
-function MenuBar() {
-  return (
-    <div className="flex justify-start items-center space-x-2 pb-4">
-      <p className="text-white font-semibold">Swap</p>
-      <Button variant="link" size="md">Limit</Button>
-    </div>
-  );
-}
-
-function TradeInfo() {
-  return (
-    <div className="py-4 text-white flex justify-between items-center">
-      <div className="flex flex-col space-y-1">
-        <p className="text-xs text-gray-400">Token Cost</p>
-        <p className="text-xs text-gray-400">Token Cost</p>
-        <p className="text-xs text-gray-400">Transaction Cost</p>
-        <p className="text-xs text-gray-400">Route</p>
-      </div>
-      <div className="flex flex-col space-y-1 text-right">
-        <p className="text-xs">~$0</p>
-        <p className="text-xs">~$0</p>
-        <p className="text-xs">~$12.23</p>
-        <p className="text-xs">Trade Route</p>
-      </div>
-    </div>
-  );
-}
 
 export default function Swapper() {
-  const { isConnected } = useAccount();
-  const [isLoading, setIsLoading] = useState(false);
-  const [tokenReverse, setTokenReverse] = useState(false);
-  const { tokenIn, tokenOut, setTradeToken } = useTradeStore();
+  const { address, isConnected } = useAccount();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [tokenInBalance, setTokenInBalance] = useState<string | null>('');
+  const {
+    chain,
+    tokenIn,
+    tokenOut,
+    estimate,
+    gas,
+    setTradeToken,
+    setTradeGas,
+    setTradePrice,
+    setTradeAmount,
+    setTradeEstimate,
+  } = useTradeStore();
+
+  const tradeAPI =
+    chain === 1
+      ? 'https://api.0x.org'
+      : 'https:/goerli.api.0x.org';
+
+  const alchemyNetwork =
+    chain === 1
+      ? Network.ETH_MAINNET
+      : Network.ETH_GOERLI;
+
+  const alchemyKey =
+    chain === 1
+      ? process.env.NEXT_PUBLIC_ALCHEMY_API_KEY_MAINNET
+      : process.env.NEXT_PUBLIC_ALCHEMY_API_KEY_GOERLI;
+
+  async function fetchPrice(amount: string) {
+    setIsLoading(true);
+    const tradeParams = {
+      sellToken: `${tokenIn.token.address}`,
+      buyToken: `${tokenOut.token.address}`,
+      sellAmount: `${Number(amount) * (10 ** tokenIn.token.decimals)}`,
+      takerAddress: address,
+    };
+
+    const priceRes = await fetch(`${tradeAPI}/swap/v1/price?${qs.stringify(tradeParams)}`);
+    // const quoteRes = await fetch(`${tradeAPI}/swap/v1/quote?${qs.stringify(tradeParams)}`);
+    const priceResult = await priceRes.json();
+    if (priceRes.status === 200) {
+      const { buyAmount, gasPrice, gas } = priceResult;
+      const tokenOutAmount = `${(
+        Number(buyAmount) /
+        10 ** tokenOut.token.decimals
+      ).toFixed(4)}`;
+
+      setTradeAmount('buy', tokenOutAmount);
+      setTradeGas(`${Number(gasPrice) / 10 ** 9}`, gas);
+      setTradeEstimate('buy', { amount: '0', price: '0' });
+      setTradeEstimate('sell', { amount: '0', price: '0' });
+    } else {
+      setTradeAmount('buy', '0');
+    }
+    setTimeout(() => setIsLoading(false), 500);
+  }
+
+  async function fetchTokenInBalance() {
+    const settings = {
+      apiKey: alchemyKey,
+      network: alchemyNetwork,
+    };
+    const alchemy = new Alchemy(settings);
+    const res = await alchemy.core.getTokenBalances(
+      `${address}`,
+      [tokenIn.token.address],
+    );
+    const rawBalance = BigInt(`${res.tokenBalances[0].tokenBalance}`).toString();
+    const balance = (Number(rawBalance) / (10 ** tokenIn.token.decimals)).toFixed(4).toString();
+
+    if (balance === '0.0000') setTokenInBalance('0');
+    else setTokenInBalance(balance);
+  }
+
+  async function fetchTradeData(value: string) {
+    if (value === '') setTradeAmount('sell', '0');
+    else setTradeAmount('sell', value);
+
+    await fetchPrice(value);
+    await fetchTokenInBalance();
+  };
 
   function setTokenInMax() {
     // tokenIn.amount = tokenIn.balance;
@@ -51,30 +109,69 @@ export default function Swapper() {
     setTradeToken('buy', tokenCache.token);
     setTimeout(() => setIsLoading(false), 1000);
   }
+  function swap() {
 
+  }
 
   return (
-    <Card>
-      <MenuBar />
-      <TokenInput
-        type="sell"
-        token={tokenIn}
-        onMax={setTokenInMax}
-        loading={isLoading}
-        onReverse={reverseToken}
-      />
-      <TokenReverse onReverse={reverseToken} />
-      <TokenInput
-        type="buy"
-        token={tokenOut}
-        loading={isLoading}
-        onReverse={reverseToken}
-      />
-      <TradeInfo />
-      {/* TODO: disabled if not connected */}
-      <Button className="w-full" colorScheme="blue">
-        Button
-      </Button>
-    </Card>
+    <div className="pt-14">
+      <Card>
+        <TokenInput
+          type="sell"
+          token={{
+            token: tokenIn.token,
+            amount: tokenIn.amount,
+            price: tokenIn.price,
+            balance: tokenInBalance,
+          }}
+          onMax={setTokenInMax}
+          loading={isLoading}
+          onInput={fetchTradeData}
+          onReverse={reverseToken}
+        />
+        <TokenReverse onReverse={reverseToken} />
+        <TokenInput
+          type="buy"
+          token={{
+            token: tokenOut.token,
+            amount: tokenOut.amount,
+            price: tokenOut.price,
+          }}
+          loading={isLoading}
+          onReverse={reverseToken}
+        />
+        <TradeEstimate />
+        {isConnected ? (
+          <Button className="w-full" colorScheme="blue">
+            Swap
+          </Button>
+        ) : (
+          <Button className="w-full" colorScheme="blue">
+            Connect Wallet
+          </Button>
+        )}
+      </Card>
+    </div>
+  );
+}
+
+function TradeEstimate() {
+  const { estimate, tokenIn, tokenOut, gas } = useTradeStore();
+
+  return (
+    <div className="rounded-[0.45rem] bg-gradient-to-r from-transparent via-[#0f172b70] to-transparent mt-2 mb-3 px-4 py-1 text-white flex justify-between items-center">
+      <div className="w-2/3 flex flex-col space-y-2">
+        <div className="flex justify-start items-center space-x-1">
+          <InformationCircleIcon className="text-white h-5 w-5 mr-1" />
+          <span className="text-xs font-semibold text-white">{`1 ${tokenIn.token.symbol}`}</span>
+          <span className="text-xs font-semibold text-slate-600"> = </span>
+          <span className="text-xs font-semibold text-white">{`${estimate.amount} ${tokenOut.token.symbol}`}</span>
+          <span className="text-xs text-slate-500">{`($${estimate.price})`}</span>
+        </div>
+      </div>
+      <div className="w-1/3 flex flex-col space-y-2 text-right">
+        <p className="text-xs">{`${gas.gasPrice} gwei`}</p>
+      </div>
+    </div>
   );
 }
